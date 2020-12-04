7c904ec8016cb1a586d3962ec578e19ed2610adc
Rik Theunis <rik.theunis@kbc.be>
2020-12-04 09:11:39 +0100

    Fix test

--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/service/shoppinglist/ShoppingListServiceTest.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/service/shoppinglist/ShoppingListServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.ezgroceries.shoppinglist.repository.cocktail.CocktailRepository;
 import com.ezgroceries.shoppinglist.repository.cocktail.entity.Cocktail;
 import com.ezgroceries.shoppinglist.repository.shoppinglist.ShoppingListRepository;
 import com.ezgroceries.shoppinglist.repository.shoppinglist.entity.ShoppingList;
@@ -24,6 +25,9 @@ class ShoppingListServiceTest {
 
     @Mock
     private ShoppingListRepository shoppingListRepository;
+
+    @Mock
+    private CocktailRepository cocktailRepository;
 
     @InjectMocks
     private ShoppingListService shoppingListService;
@@ -50,32 +54,32 @@ class ShoppingListServiceTest {
         ShoppingList mockShoppingList = new ShoppingList();
         mockShoppingList.setId(UUID.fromString("ff67c05b-5e98-4ea4-8db3-c4b106188063"));
         mockShoppingList.setName("Testname");
+        mockShoppingList.setCocktails(new HashSet<>());
 
         ShoppingList mockShoppingListAfterSave = new ShoppingList();
         mockShoppingListAfterSave.setId(UUID.fromString("ff67c05b-5e98-4ea4-8db3-c4b106188063"));
         mockShoppingListAfterSave.setName("Testname");
         Cocktail cocktail1 = new Cocktail();
         cocktail1.setId(UUID.fromString("6e4f8d41-16fc-4c99-99a1-87e6079c228e"));
-        Cocktail cocktail2 = new Cocktail();
-        cocktail2.setId(UUID.fromString("ab8b8d12-d8e9-46a6-bde2-8da3ddb0bd18"));
-        mockShoppingListAfterSave.setCocktails(new HashSet<>(Arrays.asList(cocktail1, cocktail2)));
+        mockShoppingListAfterSave.setCocktails(new HashSet<>(Arrays.asList(cocktail1)));
 
         // mock
         when(shoppingListRepository.getOne(UUID.fromString("ff67c05b-5e98-4ea4-8db3-c4b106188063"))).thenReturn(mockShoppingList);
-        when(shoppingListRepository.save(mockShoppingList)).thenReturn(mockShoppingList);
+        when(cocktailRepository.getOne(any())).thenReturn(cocktail1);
+        when(shoppingListRepository.save(any(ShoppingList.class))).thenReturn(mockShoppingListAfterSave);
 
         // perform call
         ShoppingListResource actualShoppingListResource = shoppingListService.addCocktailsToShoppingList(
                 UUID.fromString("ff67c05b-5e98-4ea4-8db3-c4b106188063"),
                 Arrays.asList(
-                        UUID.fromString("6e4f8d41-16fc-4c99-99a1-87e6079c228e"),
-                        UUID.fromString("ab8b8d12-d8e9-46a6-bde2-8da3ddb0bd18")
+                        UUID.fromString("6e4f8d41-16fc-4c99-99a1-87e6079c228e")
                 )
         );
 
         // verify
         assertEquals(UUID.fromString("ff67c05b-5e98-4ea4-8db3-c4b106188063"), actualShoppingListResource.getShoppingListId());
         assertEquals("Testname", actualShoppingListResource.getName());
+        assertEquals(1, actualShoppingListResource.getCocktails().size());
     }
 
     @Test
